5a8a10fbfe4639ad93951a9ac7739ca2355a7dd0
siliang <szhang@endiag.com>
2022-12-13 15:57:28 -0800

    add signal handler

--- signal_handler/src/main.rs
+++ signal_handler/src/main.rs
@@ -1,3 +1,8 @@
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 fn main() {
-    println!("Hello, world!");
+    let term = Arc::new(AtomicBool::new(false));
+    signal_hook::flag::register(signal_hook::consts::SIGTERM, term.clone()).unwrap();
+    while !term.load(Ordering::Relaxed) {}
+    println!("process is terminated by signal");
 }
