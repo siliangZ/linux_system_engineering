50de83e00fd5866935a3c0ae00802ef3177b6061
siliang <szhang@endiag.com>
2022-12-13 16:07:26 -0800

    update

--- signal_handler/src/main.rs
+++ signal_handler/src/main.rs
@@ -1,8 +1,19 @@
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
-fn main() {
+
+fn term_signal_handler() {
+    println!("received term signal");
+}
+
+fn register_signal_handler_unsafe() {
+    unsafe {
+        signal_hook::low_level::register(signal_hook::consts::SIGTERM, term_signal_handler)
+            .unwrap();
+    }
+}
+
+fn register_signal() {
     let term = Arc::new(AtomicBool::new(false));
     signal_hook::flag::register(signal_hook::consts::SIGTERM, term.clone()).unwrap();
-    while !term.load(Ordering::Relaxed) {}
-    println!("process is terminated by signal");
 }
+fn main() {}
