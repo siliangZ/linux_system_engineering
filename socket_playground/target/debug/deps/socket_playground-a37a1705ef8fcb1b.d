79289a1b9ca408cd2f093d90e763e8a02951d41c
siliang <szhang@endiag.com>
2022-12-23 11:02:49 -0800

    remove unused folder

--- socket_playground/target/debug/deps/socket_playground-a37a1705ef8fcb1b.d
+++
@@ -1,5 +0,0 @@
-/home/siliang/NVME/linux_system_engineering/socket_playground/target/debug/deps/socket_playground-a37a1705ef8fcb1b.rmeta: src/main.rs
-
-/home/siliang/NVME/linux_system_engineering/socket_playground/target/debug/deps/socket_playground-a37a1705ef8fcb1b.d: src/main.rs
-
-src/main.rs:
